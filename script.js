f7a2badae21b632b878b5643c91bba6a9b14f638
sameer <sameer.ghawana@gmail.com>
2021-09-08 14:26:51 +0530

    completetes game

--- script.js
+++ script.js
@@ -11,45 +11,60 @@ function playRound(playerSelection,computerSelection) {
     let result = "";
     
     if (computerSelection === "Rock") {
-        if (playerSelection === "rock"){
+        if (playerSelection === "Rock"){
             result = "Draw";
-        } else if (playerSelection === "paper") {
+        } else if (playerSelection === "Paper") {
             result = "Won";
-        } else if (playerSelection === "scissors") {
+        } else if (playerSelection === "Scissors") {
             result = "Lose";
         }
     } else if (computerSelection === "Paper") {
-        if (playerSelection === "rock"){
+        if (playerSelection === "Rock"){
             result = "Lose";
-        } else if (playerSelection === "paper") {
+        } else if (playerSelection === "Paper") {
             result = "Draw"
-        } else if (playerSelection === "scissors") {
+        } else if (playerSelection === "Scissors") {
             result = "Won";
         }
     } else if (computerSelection === "Scissors") {
-        if (playerSelection === "rock"){
+        if (playerSelection === "Rock"){
             result = "Won";
-        } else if (playerSelection === "paper") {
+        } else if (playerSelection === "Paper") {
             result = "Lose";
-        } else if (playerSelection === "scissors") {
+        } else if (playerSelection === "Scissors") {
             result = "Draw";
         }
     }
+    return result;
+}
 
-    if (result === "Draw") {
-        return ("Its a Draw!");
-    } else if (result === "Won"){
-        return (`You ${result}! ${playerSelection} beats ${computerSelection}`)
-    } else if (result === "Lose") {
-        return (`You ${result}! ${computerSelection} beats ${playerSelection}`)
+function inputProcessing() {
+    playerInput = prompt("Rock,Paper or Scissors?");
+    playerInput = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
+    return playerInput
+}
+
+
+
+function game () {
+    let playerScore = 0; let computerScore = 0;
+    while(true){
+        let input = inputProcessing();
+        let result = playRound(input,computerPlay());
+        if (result === "Won"){
+            playerScore++
+            console.log(`The current score is Player ${playerScore} vs Computer ${computerScore}`)
+        } else if (result === "Lose") {
+            computerScore++
+            console.log(`the current score is Player ${playerScore} vs Computer ${computerScore}`)
+        }
+
+        if ((playerScore === 5)||(computerScore === 5)){
+            console.log(`The game is over final score is Player - ${playerScore} vs Computer ${computerScore}`)
+            break;
+        }
     }
 }
 
-function game() {
-    let count = 0;
-    for (let i=0;i<5;i++){
-        let playerInput = prompt(`(Round ${count + 1}) Rock,Paper or Scissors?`);
-        play
-    }
-}
+game();
 
